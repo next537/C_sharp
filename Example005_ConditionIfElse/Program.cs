1aec652580f57db1a51efeabf8267418bd859999
next215 <next215@gmail.com>
2022-10-05 14:33:21 +0300

    add Маша

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username == "Маша")
+{
+    //Console.ReadLine("Ура, это же МАША");
+    Console.WriteLine("Ура, это же Маша");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
